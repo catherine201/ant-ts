61cbf708f496192ac24b623dfe9e76a08ade05f1
sinceway <sinceway@sincewaydeMacBook-Pro.local>
2019-04-13 17:07:21 +0800

    all

---
+++ config-overrides.js
@@ -0,0 +1,6 @@
+const { override, fixBabelImports } = require('customize-cra')
+
+module.exports = function override(config, env) {
+  // do stuff with the webpack config...
+  return config
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Button from 'antd/lib/button'
+import './App.css'
 
 class App extends Component {
-  render() {
+  public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Button type="primary">Button</Button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
