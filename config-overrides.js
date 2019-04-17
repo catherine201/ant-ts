8f4b9266f3c570493d7f6389abbd449919626873
sinceway <sinceway@sincewaydeMacBook-Pro.local>
2019-04-17 18:04:16 +0800

    ts

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,15 @@
-const { override, fixBabelImports } = require('customize-cra')
+const { override, fixBabelImports, addLessLoader, addWebpackAlias } = require('customize-cra')
+const path = require('path')
 
-module.exports = function override(config, env) {
-  // do stuff with the webpack config...
-  return config
-}
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { '@primary-color': '#1DA57A' }
+  }),
+  addWebpackAlias({ '@': path.resolve(__dirname, 'src') })
+)
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
-import React, { Component } from 'react'
-import Button from 'antd/lib/button'
-import './App.css'
+import React, { useState, useEffect } from 'react'
+import { Button } from 'antd'
+import styles from '@/App.module.less'
 
-class App extends Component {
-  public render() {
-    return (
-      <div className="App">
-        <Button type="primary">Button</Button>
-      </div>
-    )
-  }
+// class App extends Component {
+//   public render() {
+//     return (
+//       <div className={styles.App}>
+//         <Button type="primary">Button</Button>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
+
+export const App = () => {
+  const [state, setState] = useState({ ...state, username: 'scq000' })
+  const [myName, setMyName] = useState('catherine')
+
+  useEffect(() => {
+    console.log('componentDidMount: 组件加载后')
+    return () => {
+      console.log('componentWillUnmount: 组件卸载， 做一些清理工作')
+    }
+  }, [])
+
+  useEffect(() => {
+    console.log('componentDidUpdate： 更新usernmae')
+  }, [state.username])
+
+  return (
+    <div className={styles.App}>
+      <p>Welcome to homepage. {state.username}</p>
+      <p>Welcome. {state.myName}</p>
+      <input
+        type="text"
+        placeholder="input a username"
+        onChange={event => setState({ username: event.target.value })}
+      />
+      <Button type="primary">Button</Button>
+    </div>
+  )
 }
-
-export default App
